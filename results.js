e739042ab4a9cad88b248066d49697ece4c22e3e
famousj <leblanc@wustl.edu>
2014-03-05 07:33:32 -0600

    Created a results page, still a stub.  One result: '27 Yankees.

---
+++ results.js
@@ -0,0 +1,8 @@
+exports.results = [
+	{
+		topic: "What historic sports team are you?",
+        img: "imgs/27_yankees.jpg",
+	},
+];
+
+
--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var logfmt	= require("logfmt");
 var app		= express();
 var path	= require('path');
 
-var questions = require('./questions.js');
+var questions   = require('./questions.js');
+var results     = require('./results.js');
 
 app.use(logfmt.requestLogger());
 
@@ -14,7 +15,7 @@ var do404 = function(res) {
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
+app.get('/(index.html)?', function(req, res) {
 	res.render('index.jade', {title: "What the What?  The Ultimate Web Quiz"});
 });
 
@@ -38,18 +39,21 @@ app.get(/^\/q(\d+).html$/, function(req, res) {
 	}
 	else if (qnum == questions.mcStart + questions.mc.length) {
 		var question = questions.pictureQuestions[1];
-		question.nextpage = nextpage;
+		question.nextpage = "results.html";
 		res.render('pictures.jade', question);
-	}
-	else if (qnum == questions.mc.length + questions.pictureQuestions.length) {
-		// Do the results here.
-
 	}
 	else { 
 		do404(res);
 	}
 
 });
+
+app.get("/results.html", function(req, res) {
+
+	res.render('results.jade', results.results[0]);
+});
+
+
 
 var port = Number(process.env.PORT || 2727);
 app.listen(port, function() {
